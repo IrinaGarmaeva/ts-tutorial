31c9230a38db637e819b3eea177937f33819d0ca
Irina Garmaeva <garmaeva-i@mail.ru>
2023-12-05 22:03:43 +0100

    Add forwardRef to Input component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
+import {useRef} from 'react'
 import Input from "./components/Input";
 import Button from "./components/Button";
 import Container from "./components/Container";
 
 function App() {
+  const input = useRef(null)
+
   return(
     <main>
       <Input id="name" label="Your name"/>
       <Input id="age" label="Your age" type="number"/>
+      <Input id="test" label="Test input" ref={input}/>
       <p>
         <Button>A button</Button>
       </p>
