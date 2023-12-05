76dbf51b5de9d0bacf09fb8272a1f29d4293714a
Irina Garmaeva <garmaeva-i@mail.ru>
2023-12-05 21:14:45 +0100

    Feature: add button component, working with type predicates

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import Input from "./components/Input";
+import Button from "./components/Button";
 
 function App() {
   return(
     <main>
       <Input id="name" label="Your name"/>
       <Input id="age" label="Your age" type="number"/>
+      <p>
+        <Button>A button</Button>
+      </p>
+      <p>
+      <Button href="https://google.com" target="_blank">A link</Button>
+      </p>
     </main>
   );
 }
