f91992b56af56e4a6fdc283fb9b69c181716282c
Irina Garmaeva <garmaeva-i@mail.ru>
2023-12-11 11:05:38 +0100

    project3_data-fetching
    fetching data from JSONPlaceholder

--- project3_data-fetching/src/App.tsx
+++ project3_data-fetching/src/App.tsx
@@ -1,5 +1,51 @@
+import { useEffect, useState, type ReactNode } from "react";
+import { get } from "./utils/http";
+import { type BlogPost } from "./components/BlogPosts";
+import BlogPosts from "./components/BlogPosts";
+import fetchingImg from './assets/data-fetching.png'
+
+type RawDataBlogPost = {
+  id: number;
+  title: string;
+  body: string;
+  userId: number;
+};
+
 function App() {
-  return <h1>Data Fetching!</h1>;
+  const [fetchedPosts, setFetchedPosts] = useState<BlogPost[]>();
+
+  useEffect(() => {
+    async function fetchPosts() {
+      const data = (await get(
+        "https://jsonplaceholder.typicode.com/posts"
+      )) as RawDataBlogPost[];
+
+      const blogPosts: BlogPost[] = data.map((rawPost) => {
+        return {
+          id: rawPost.id,
+          title: rawPost.title,
+          text: rawPost.body,
+        };
+      });
+
+      setFetchedPosts(blogPosts);
+    }
+
+    fetchPosts();
+  }, []);
+
+  let content : ReactNode
+
+  if (fetchedPosts) {
+    content =   <BlogPosts posts={fetchedPosts} />
+  }
+
+  return (
+    <main>
+      <img src={fetchingImg} alt="An abstract image depicting a data fetching process" />
+      {content}
+    </main>
+  );
 }
 
 export default App;
