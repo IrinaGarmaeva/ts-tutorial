590955ea698883f5103026c1707cb119f138c02d
Irina Garmaeva <garmaeva-i@mail.ru>
2023-12-11 11:25:24 +0100

    project3_data-fetching
    handling loading and error states

--- project3_data-fetching/src/App.tsx
+++ project3_data-fetching/src/App.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState, type ReactNode } from "react";
 import { get } from "./utils/http";
 import { type BlogPost } from "./components/BlogPosts";
 import BlogPosts from "./components/BlogPosts";
+import ErrorMessage from "./components/ErrorMessage";
 import fetchingImg from './assets/data-fetching.png'
 
 type RawDataBlogPost = {
@@ -13,22 +14,32 @@ type RawDataBlogPost = {
 
 function App() {
   const [fetchedPosts, setFetchedPosts] = useState<BlogPost[]>();
+  const [isFetching, setIsFetching] = useState<boolean>(false)
+  const [error, setError] = useState<string>()
 
   useEffect(() => {
     async function fetchPosts() {
-      const data = (await get(
-        "https://jsonplaceholder.typicode.com/posts"
-      )) as RawDataBlogPost[];
+      setIsFetching(true)
+      try {
+        const data = (await get(
+          "https://jsonplaceholder.typicode.com/posts"
+        )) as RawDataBlogPost[];
 
-      const blogPosts: BlogPost[] = data.map((rawPost) => {
-        return {
-          id: rawPost.id,
-          title: rawPost.title,
-          text: rawPost.body,
-        };
-      });
-
-      setFetchedPosts(blogPosts);
+        const blogPosts: BlogPost[] = data.map((rawPost) => {
+          return {
+            id: rawPost.id,
+            title: rawPost.title,
+            text: rawPost.body,
+          };
+        });
+  
+        setIsFetching(false)
+        setFetchedPosts(blogPosts);
+      } catch (error) {
+        setIsFetching(false)
+        setError((error as Error).message)
+      }
+      
     }
 
     fetchPosts();
@@ -38,6 +49,14 @@ function App() {
 
   if (fetchedPosts) {
     content =   <BlogPosts posts={fetchedPosts} />
+  }
+
+  if(isFetching) {
+    content= <p id="loading-fallback">Fetching posts...</p>
+  }
+
+  if(error) {
+    content = <ErrorMessage text={error}/>
   }
 
   return (
