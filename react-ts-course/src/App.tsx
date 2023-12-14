d537ff9ddc045b194f44c3d5fbefe5a77943eeea
Irina Garmaeva <garmaeva-i@mail.ru>
2023-12-14 19:25:48 +0100

    react-ts-course
    add multipurpose component List

--- react-ts-course/src/App.tsx
+++ react-ts-course/src/App.tsx
@@ -1,8 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import Card, { CardVariant } from './components/Card';
 import UserList from './components/UserList';
+import List from './components/List';
 import { IUser } from './types/types';
 import axios from 'axios';
+import UserItem from './components/UserItem';
 
 function App() {
 const [users, setUsers] = useState<IUser[]>([])
@@ -10,7 +12,7 @@ const [users, setUsers] = useState<IUser[]>([])
 async function fetchUsers() {
   try {
     const response = await axios.get<IUser[]>('https://jsonplaceholder.typicode.com/users/')
-
+    console.log(response.data)
     setUsers(response.data)
   } catch (e) {
     alert(e)
@@ -26,7 +28,7 @@ useEffect(() => {
       <Card width='200px' height='200px' variant={CardVariant.outlined} onClick={() => window.alert("You clicked the card")}>
         <button>Button</button>
       </Card>
-      <UserList users={users}/>
+      <List items={users} renderItem={(user: IUser) => <UserItem user={user} key={user.id} />}/>
     </div>
   );
 }
