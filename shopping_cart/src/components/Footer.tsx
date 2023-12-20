f8669e83db456cf4e2a221de4244bc39ed8ba1f4
Irina Garmaeva <garmaeva-i@mail.ru>
2023-12-20 16:24:13 +0100

    project shopping_cart
    add useCart hook, Footer component

--- shopping_cart/src/components/Footer.tsx
+++ shopping_cart/src/components/Footer.tsx
@@ -1,13 +1,29 @@
-import React from 'react'
+import React from "react";
+import useCart from "../hooks/usecart";
 
 type FooterProps = {
-	viewCart: boolean
-}
+  viewCart: boolean;
+};
 
-const Footer = ({viewCart}: FooterProps) => {
-	return (
-		<div>Footer</div>
-	)
-}
+const Footer = ({ viewCart }: FooterProps) => {
+  const { totalItems, totalPrice } = useCart();
+  const year: number = new Date().getFullYear();
 
-export default Footer
+  const pageContent = viewCart ? (
+    <p>Shopping Cart &copy; {year}</p>
+  ) : (
+    <>
+      <p>Total Items: {totalItems}</p>
+      <p>Total Items: {totalPrice}</p>
+			<p>Shopping Cart &copy; {year}</p>
+    </>
+  );
+
+  return (
+		<footer>
+			{pageContent}
+		</footer>
+	);
+};
+
+export default Footer;
--- shopping_cart/src/components/Header.tsx
+++ shopping_cart/src/components/Header.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Navbar from "./Navbar";
+import useCart from "../hooks/usecart";
 
 export type PropsType = {
   viewCart: boolean;
@@ -7,13 +8,15 @@ export type PropsType = {
 };
 
 const Header = ({ viewCart, setViewCart }: PropsType) => {
+	const {totalItems, totalPrice} = useCart()
+
   return (
     <header className="header">
       <div className="header__title-bar">
         <h1>Watches Company</h1>
         <div className="header__price-box">
-          <p>Total Items:</p>
-          <p>Total Items:</p>
+          <p>Total Items: {totalItems}</p>
+          <p>Total Price: {totalPrice}</p>
         </div>
       </div>
       <Navbar viewCart={viewCart} setViewCart={setViewCart} />
